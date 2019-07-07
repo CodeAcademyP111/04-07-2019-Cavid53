6f84d0e235be45510b66b23ca02a3d17f6b9c2ca
cavid53 <JavidJb@code.edu.az>
2019-07-07 11:41:39 +0400

    homework_extention

---
+++ homework_extention_07.04.19/homework_extention_07.04.19/Extention.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_extention_07._04._19
+{
+     public static class Extention
+    {
+        //exercise-1
+        #region extention for find simple number
+        //public static string SimpleNumber( this int num)
+        //  {
+        //      int n = 0;
+        //      for (int i = 1; i <= num; i++)
+        //      {
+        //          if (num % i == 0)
+        //          {
+        //              n++;
+        //          }
+        //      }
+
+        //      if (n == 2)
+        //      {
+        //          return $"This is simple number";
+        //      }
+        //      else
+        //      {
+        //          return $"This is  not simple number";
+        //      }
+        //  }
+        #endregion
+
+
+            //exercise-2
+        public static int FindMyIndexOf( this string word1,char a)
+        {
+            int n = -1;
+            for (int i = 0; i < word1.Length; i++)
+
+            {
+                if (word1[i] == a) 
+                {
+                    n = i;
+                }
+            }
+            return n;
+        }
+    }
+}
---
+++ homework_extention_07.04.19/homework_extention_07.04.19/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_extention_07._04._19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region extention for find simple number
+            //Console.WriteLine("Please enter the number !");
+            //int a = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(a.SimpleNumber());
+            #endregion
+
+
+            string word = "Azerbaycan";
+            Console.WriteLine(word.FindMyIndexOf('a'));
+           
+            
+
+        }
+    }
+}
